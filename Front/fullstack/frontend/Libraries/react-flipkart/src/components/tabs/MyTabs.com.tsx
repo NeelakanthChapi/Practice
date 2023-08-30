c22e90696e51263443b9dd77dcb53f16199eeef4
NeelakanthChapi <chapineelakanth@gmail.com>
2023-08-31 00:53:37 +0530

    mui

---
+++ Front/fullstack/frontend/Libraries/react-flipkart/src/components/tabs/MyTabs.com.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+import Box from '@mui/material/Box';
+import { TabList, TabPanel } from '@mui/lab';
+import { TabContext } from '@mui/lab';
+import { MyProps } from '../../pages/lessons/props/MyProps';
+import { H1 } from '../../pages/lessons/elements/H1';
+import { MyButton } from '../button/MyButton.com';
+import { MyRatings } from '../ratting/MyRatings.com';
+
+import { MyTooltip } from '../tooltit/MyTooltip.com';
+import { MyTextield } from '../textfield/MyTextield.com';
+export let MyTabs:React.FC<{}>=()=>{
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+console.log("newValue",typeof newValue)
+    setValue(`${newValue}`);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="props" value="1" />
+            <Tab label="Elemetnts" value="2" />
+            <Tab label="Textfield" value="3" />
+            <Tab label="tooltip" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><MyProps/></TabPanel>
+        <TabPanel value="2"><H1/></TabPanel>
+        <TabPanel value="3"><MyTextield/></TabPanel>
+        <TabPanel value="4"><MyTooltip/></TabPanel>
+        <TabPanel value='5'>
+        
+
+<MyButton/>
+<MyRatings/>
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
---
+++ Front/fullstack/frontend/Libraries/react-flipkart/src/components/tooltit/MyTooltip.com.tsx
@@ -0,0 +1,12 @@
+import { Rating, Tooltip, Typography } from "@mui/material"
+
+export const MyTooltip:React.FC<{}>=()=>{
+    return <div>
+  
+<Rating name="read-only" value={2} readOnly />
+<Typography component="legend">Disabled</Typography>
+<Rating name="disabled" value={3} disabled />
+<Typography component="legend">No rating given</Typography>
+<Rating name="no-value" value={null} />
+    </div>
+}
