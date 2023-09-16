b76d011f6ed78869ad91f0467974473a59d75e8e
NeelakanthChapi <chapineelakanth@gmail.com>
2023-09-16 15:06:16 +0530

    mysqlcompleted

---
+++ Front/fullstack/DataBase/My-sql/find.ts
@@ -0,0 +1,16 @@
+var mysql=require('mysql')
+var con=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'neel'
+})
+con.connect((err:any)=>{
+    if(err)throw err;
+    console.log('db connected')
+let query="SELECT * FROM student"
+con.query(query,(err:any,res:any)=>{
+    if(err)throw err
+    console.log(res)  
+})
+})
