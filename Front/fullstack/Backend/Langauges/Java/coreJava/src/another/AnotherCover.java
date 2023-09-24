15bb9fe488776a09e38373eb85d930e4f2e49adc
NeelakanthChapi <chapineelakanth@gmail.com>
2023-09-25 00:12:36 +0530

    java datatypes

---
+++ Front/fullstack/Backend/Langauges/Java/coreJava/src/another/AnotherCover.java
@@ -0,0 +1,6 @@
+package another;
+
+public class AnotherCover {
+	String cover1="plastic cover";
+}
+class DefaultMode2{}
---
+++ Front/fullstack/Backend/Langauges/Java/coreJava/src/basics_java/MyownDatatype.java
@@ -0,0 +1,18 @@
+package basics_java;
+
+public class MyownDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("im running");
+Human ravi=new Human();
+
+
+	}
+
+}
+
+class Human{
+	String eyes="2";
+	void witheyes(){}
+}
